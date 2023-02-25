0002542a3015f60efc6821fcfb88dfd6a526c1f3
teckzong <teckzong_1996@hotmail.com>
2023-02-25 15:08:27 +0800

    refactor and fix transactionRoutes

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -5,34 +5,24 @@ const con = require('../connection.js');
 
 router.post('/', (req, res) => {
     const {ClaimID} = req.body;
-    console.log(ClaimID);
 
-
-    con.query("SELECT Status FROM insuranceclaims WHERE ClaimID = ?", ClaimID,(err, claim_status) =>{
+    con.query("DELETE FROM InsuranceClaims WHERE claimID = ? AND Status != 'Approved' "
+    ,ClaimID, (err, result) =>{
         if(err){
             res.send(err);
+            console.log("not deleted");
         } else {
-            if (claim_status.status == 'Approved'){
-                return res.send({
-                    message : "Approved claim cannot be deleted."
-                });
-            } else if (claim_status.status === null){
-                return res.send({
-                    message : "Claim does not exist."
-                });
+            if(result.affectedRows == 0){
+                console.log("cannot delete row");
+                res.send(result);
+            } else {
+                console.log("deleted");
+                res.send(result);
             }
 
-            con.query("DELETE FROM insuranceclaims WHERE ClaimID = ?",ClaimID, (err, result) =>{
-                if(err){
-                    res.send(err);
-                } else {
-                    res.send(result);
-                }
-            });
+            
         }
     });
-
 });
 
 module.exports = router;
-
