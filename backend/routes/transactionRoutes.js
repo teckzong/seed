e61bf59bb64347e09270c50de1a625053efb6dcc
teckzong <teckzong_1996@hotmail.com>
2023-02-25 16:50:00 +0800

    refactor insertclaim

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -11,152 +11,27 @@ const requireAuth = require('../middlewares/requireAuth');
 
 router.post('/', (req, res) => {
     const claim = req.body;
-
+    console.log(claim);
     if(!claim){
         console.log("err")
         //return res.status(422).send(resResult(0, "Claim is null!"));
     }
     try{
-        con.query("Select *, Timestamp(PolicyEndDate) as PolicyEndDate_ts from InsurancePolicies where InsuranceID = ?", claim.InsuranceID, (err, ip) =>{
-            if(claim.amount > ip.ClaimLimit || claim.ExpenseDate > ip.PolicyEndDate_ts){
-                // return res.status(422).send(resResult(0, "Invalid claim, please check amount or expense date"));
-                console.log("err")
-                console.log(ip)
-                const currentDate = new Date();
-                const timestamp = currentDate.getTime();
-                con.query("select max(ClaimID) from InsuranceClaims", (err, result) =>{
-                    claim['ClaimID'] = result + 1;
-                })
-
-                claim['Status'] = "Pending";
-                claim['LastEditedClaimDate'] = timestamp;
-                con.query("INSERT INTO InsuranceClaims value ?", claim, (err, result) =>{
-                    if(err){
-                        console.log("err")
-                        //return res.status(422).send(resResult(0, "Save DB error"));//err.message
-                    } else {
-                        //res.send(resResult(1, 'Successfully create claim', null));
-                        console.log("saved")
-                    }
+        con.query("INSERT INTO insuranceclaims SET ?", claim, (err, result) =>{
+            if(err){
+                res.send(err);
+            } else {
+                res.send({
+                    info : result,
+                    res: "User registered"
                 });
             }
-        })
+        });
     }
     catch(err){
         console.log(err)
     }
 
-        //return res.status(422).send(resResult(0, "Save DB error"));//err.message
-
-
 });
-
-/**
- * edit
- *
- * // router.put('/:id', (req, res) => {
- * //     var user = users.find(c => c.id === parseInt(req.params.id));
- * //     if(!user) res.status(404).send('User with given ID not found');
- * //     user.name = req.body.name;
- * //     res.send({
- * //         info : user
- * //     });
- * // });
- */
-router.post('/edit-claim/:cid', (req, res) => {
-    const cid = req.params.id;
-    //const claim = find by cid
-    if(!claim){
-        return res.status(422).send(resResult(0, "Cannot find in db"));//err.message
-    }
-    try{
-        //edit status
-    }
-    catch (err){
-        return res.status(422).send(resResult(0, "Invalid Editing"));//err.message
-    }
-});
-
-/**
- * remove
- *
- * // router.put('/:id', (req, res) => {
- * //     var user = users.find(c => c.id === parseInt(req.params.id));
- * //     if(!user) res.status(404).send('User with given ID not found');
- * //     user.name = req.body.name;
- * //     res.send({
- * //         info : user
- * //     });
- * // });
- */
-router.post('/remove-claim/:id', (req, res) => {
-    const cid = req.params.id;
-
-    //var user = users.find(c => c.id === parseInt(req.params.id));
-    // //     if(!user) res.status(404).send('User with given ID not found');
-    // //
-    // //     const index = users.indexOf(user);
-    // //     users.splice(index, 1);
-    // //     res.send({
-    // //         info : user,
-    // //         res: "Successfully deleted"
-    // //     });
-    con.query("INSERT INTO user SET ?", cUser, (err, result) =>{
-        if(err){
-            res.send(err);
-        } else {
-            res.send({
-                info : result,
-                res: "User registered"
-            });
-        }
-    });
-});
-// router.get('/', (req, res) => {
-//     con.query("select * from user", (err, result) =>{
-//         if(err){
-//             res.send(err);
-//         } else {
-//             res.send(result);
-//         }
-//     });
-// });
-//
-// router.get('/:id', (req, res) => {
-//     let id = req.params.id;
-//     con.query("select * from user where EmployeeID = ?", id,(err, result) =>{
-//         if(err){
-//             res.send(err);
-//         } else {
-//             res.send({
-//                 info : result
-//             });
-//             //res.status(200).json({info : result});
-//         }
-//     });
-// });
-//
-//
-//
-// router.put('/:id', (req, res) => {
-//     var user = users.find(c => c.id === parseInt(req.params.id));
-//     if(!user) res.status(404).send('User with given ID not found');
-//     user.name = req.body.name;
-//     res.send({
-//         info : user
-//     });
-// });
-//
-// router.delete('/:id', (req, res) => {
-//     var user = users.find(c => c.id === parseInt(req.params.id));
-//     if(!user) res.status(404).send('User with given ID not found');
-//
-//     const index = users.indexOf(user);
-//     users.splice(index, 1);
-//     res.send({
-//         info : user,
-//         res: "Successfully deleted"
-//     });
-// });
 
 module.exports = router;
