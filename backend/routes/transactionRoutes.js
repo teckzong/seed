8ed195f6eeb0ce3ded0d85614242644bcfef7984
teckzong <teckzong_1996@hotmail.com>
2023-02-25 14:52:51 +0800

    Refactor transactionRoutes

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -3,30 +3,35 @@ const router = express.Router();
 const con = require('../connection.js');
 
 
-router.get('/delete-claim', (req, res) => {
+router.post('/', (req, res) => {
     const {ClaimID} = req.body;
-    con.query(`SELECT Status
-    FROM InsuranceClaims
-    WHERE claimID = ?`, ClaimID, (err, claim_status));
+    console.log(ClaimID);
 
-    if (claim_status.status == 'Approved'){
-        return res.send({
-            message : "Approved claim cannot be deleted."
-        });
-    } elif (claim_status == null){
-        return res.send({
-            message : "Claim does not exist."
-        });
-    }
 
-    con.query(`DELETE FROM InsuranceClaims WHERE claimID = ?`
-    ,ClaimID, (err, result) =>{
+    con.query("SELECT Status FROM insuranceclaims WHERE ClaimID = ?", ClaimID,(err, claim_status) =>{
         if(err){
             res.send(err);
         } else {
-            res.send(result);
+            if (claim_status.status == 'Approved'){
+                return res.send({
+                    message : "Approved claim cannot be deleted."
+                });
+            } else if (claim_status.status === null){
+                return res.send({
+                    message : "Claim does not exist."
+                });
+            }
+
+            con.query("DELETE FROM insuranceclaims WHERE ClaimID = ?",ClaimID, (err, result) =>{
+                if(err){
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
         }
     });
+
 });
 
 module.exports = router;
