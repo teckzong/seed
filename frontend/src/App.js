2800bbbd818a169ead01ce5eaa657fa73128d9ba
sohwaileong <sohwaileong@gmail.com>
2023-02-25 10:40:00 +0800

    added required library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Transaction from "./pages/Transaction";
 
 function App() {
-  return (
-    <div className="App">
-      <p>Test</p>
-    </div>
-  );
+  let router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/transactions", element: <Transaction /> },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/pages/Transaction.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Transaction = () => {
+  return <p>Transaction page</p>;
+};
+
+export default Transaction;
