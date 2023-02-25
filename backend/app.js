6938b6504d577895bcb0e66c7e0dac4a7699bf82
teckzong <teckzong_1996@hotmail.com>
2023-02-25 11:10:35 +0800

    add database connection

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const userRoutes = require('./routes/users.js');
 
 const app = express();
 //port env for listening
 const port = process.env.PORT || 3000; 
 app.use(express.json());
 
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
