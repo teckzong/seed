41d913de302c5f33d17bc96d89ca5661aebc996a
naznor <nazrinordinn@gmail.com>
2023-02-25 11:33:27 +0800

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Transaction from "./pages/Transaction";
+import Login from "./pages/Login";
 
 function App() {
   let router = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/transactions", element: <Transaction /> },
+    { path: "/login", element: <Login /> },
   ]);
   return <RouterProvider router={router} />;
 }
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { useState, useRef } from "react";
+
+const Login = () => {
+
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [newUsername, setNewUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errMsg, setErrMsg] = useState("");
+    const [success, setSuccess] = useState(false)
+
+    useEffect(() => {
+        setErrMsg("");
+    }, [newUsername, password])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(newUsername, password);
+        setNewUsername("");
+        setPassword("");
+        setSuccess(true)
+
+        return (console.log("success"));
+
+return (
+    <div className="login">
+        <p> Login to view your insurance claims and policies</p>
+        <form onSubmit={handleSubmit}>
+            <p>
+                <label>Employee ID</label>
+                <input type="text" id="username" onChange={(e) => setNewUsername(e.target.value)} autocomplete="off" value={newUsername} required></input>
+            </p>
+            <p>
+                <label>Password</label>
+                <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} autocomplete="off" value={password} required></input>
+            </p>
+
+            <button>Login</button>
+
+            <p>{newUsername}</p>
+            <p>{password}</p>
+        </form>
+    </div>
+);
+
+};
+
+export default Login;
