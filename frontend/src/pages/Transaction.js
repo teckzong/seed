8b8738fd92c4398f7431deb305bcdfe8667abbcb
Ivan <ivan.t.mailbox@gmail.com>
2023-02-25 04:09:49 +0000

    Added image

--- frontend/src/pages/Transaction.js
+++ frontend/src/pages/Transaction.js
@@ -10,14 +10,14 @@ const Transaction = () => {
               <div class="row h-5 w-5"><img src="view_claim.jpg" class="img-fluid"></img></div>
               <h5 class="card-title">Create new claim</h5>
               <p class="card-text">Create new claims to better protect you and your dependents.</p>
-              <a href="#" class="btn btn-primary">New claim</a>
+              <a href="transactions/newclaim" class="btn btn-primary">New claim</a>
             </div>
           </div>
         </div>
         <div class="col-sm-3">
           <div class="card">
             <div class="card-body">
-              <div class="row h-5 w-5"><img src="view_claim.jpg" class="img-fluid"></img></div>
+              <div class="row h-5 w-5"><img src="waiting.jpg" class="img-fluid"></img></div>
               <h5 class="card-title">Edit pending/ rejected claims</h5>
               <p class="card-text">Made a mistake with your claims? No worries!</p>
               <a href="#" class="btn btn-primary">Edit claim</a>
