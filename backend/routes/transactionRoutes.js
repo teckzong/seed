c2efa2f5d833df2c64a691094a9aa9e72d8cf843
NikomiiC <queenandyria@hotmail.com>
2023-02-25 13:42:07 +0800

    create claim

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -7,27 +7,43 @@ router.use(requireAuth);//require to sign in first
 /**
  * create
  */
+let insurancePolicy;
+function getInsurancePolicy (inId){
+    con.query("Select *, Timestamp(PolicyEndDate) as PolicyEndDate_ts from InsurancePolicies where InsuranceID = ?", inId, (err, result) =>{
+        insurancePolicy = result;
+    })
+}
 router.post('/create-claim', (req, res) => {
     const claim = req.body;
+
     if(!claim){
         return res.status(422).send(resResult(0, "Claim is null!"));
     }
-    try{
-        con.query("INSERT INTO InsuranceClaims SET ?", claim, (err, result) =>{
+
+    if(claim.amount > insurancePolicy.ClaimLimit || claim.ExpenseDate > insurancePolicy.PolicyEndDate_ts){
+        return res.status(422).send(resResult(0, "Invalid claim, please check amount or expense date"));
+    }
+
+        const currentDate = new Date();
+        const timestamp = currentDate.getTime();
+        claim['Status'] = "Pending";
+        claim['LastEditedClaimDate'] = timestamp;
+        con.query("INSERT INTO InsuranceClaims value ?", claim, (err, result) =>{
             if(err){
-                res.send(err);
+                return res.status(422).send(resResult(0, "Save DB error"));//err.message
             } else {
                 //todo decide ltr
-                res.send({
-                    info : result,
-                    res: "User registered"
-                });
+                // res.send({
+                //     info : result,
+                //     res: "User registered"
+                // });
+                res.send(resResult(1, 'Successfully create claim', null));
             }
         });
-    }
-    catch (err){
+
+
         return res.status(422).send(resResult(0, "Save DB error"));//err.message
-    }
+
 
 });
 
