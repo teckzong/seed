9c603ee9d7145b23b14a03c214edbb07916956d1
Ivan <ivan.t.mailbox@gmail.com>
2023-02-25 03:27:32 +0000

    Added transaction page icons

--- frontend/src/pages/Transaction.js
+++ frontend/src/pages/Transaction.js
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Transaction = () => {
-  return <p>Transaction page</p>;
+  return <p>
+      <div className="h-100 d-flex align-items-center justify-content-center">
+      <div class="row h-100 d-flex align-items-center justify-content-center">
+        <div class="col-sm-3">
+          <div class="card">
+            <div class="card-body">
+              <div class="row"><img src="view_claim.jpg" class=""></img></div>
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card">
+            <div class="card-body">
+              <div class="row"><img src="view_claim.jpg" class=""></img></div>
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card">
+            <div class="card-body">
+              <div class="row"><img src="view_claim.jpg" class=""></img></div>
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+    </div>
+    </div>
+  </p>;
 };
 
 export default Transaction;
